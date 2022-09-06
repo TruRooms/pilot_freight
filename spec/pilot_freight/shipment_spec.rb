2b696f0a69b0e271099a4227475fa5305db8555e
Steve <steve@trurooms.com>
2022-09-06 15:45:32 -0700

    Ready to badge

--- spec/pilot_freight/shipment_spec.rb
+++ spec/pilot_freight/shipment_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe PilotFreight::Shipment do
     context "with no attributes" do
       it "should return an empty string" do
         shipment = PilotFreight::Shipment.new({})
-        byebug
-        expect(xml).to eq("")
+        resp = shipment.rate
+        expect(resp[:code]).to eq(200)
       end
     end
   end
